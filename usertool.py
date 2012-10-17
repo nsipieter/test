9fdcba71f98cf303e420f954d36e8e1f30a0eb4b
Nicolas Sipieter <nsipieter@capensis.fr>
2012-10-17 16:38:21 +0200

    add, list et del user utilisent la couche d'abstration

--- usertool.py
+++ usertool.py
@@ -3,6 +3,7 @@ import sys, hashlib, getpass, time, argparse
 from caccount import caccount
 from cstorage import cstorage
 from pymongo import Connection
+from crecord import crecord
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
@@ -31,13 +32,26 @@ connection = Connection(args.server_link, args.server_port)
 db = connection.canopsis
 collection = db.object
 
+user_account = caccount(user="root", group="root")
+my_storage = cstorage(user_account, namespace='object', logging_level=logging.DEBUG, mongo_host=args.server_link)
 
 
 if (args.list_users):
+
+# current: user_account.cat utilise la couche d'abstraction
 	for i in collection.find():
 		if i.get('crecord_type', False) == 'account':
 			user_account = caccount(user=i['user'], group="capensis")
 			user_account.cat()
+
+
+	records = my_storage.find({'crecord_type':'account'},account=user_account)
+	for record in records:
+		record_dict = record.dump()
+		print "User: %s" % (record_dict['user'])
+		print "Mail: %s" % (record_dict['mail'])
+		print "Pass: %s" % (record_dict['shadowpasswd'])
+		print ""
 
 
 if (args.add_user):
@@ -56,6 +70,7 @@ if (args.add_user):
 		m.update(args.user_pass)
 		cpassword = m.hexdigest()
 		time_stamp = int(time.time())
+		"""
 		post = {u'aaa_access_owner': [u'r', u'w'], u'aaa_group': u'group.CPS_'+ args.user_name, 
 		u'aaa_admin_group': u'group.CPS_account_admin', u'children': [], 
 		u'firstname': u'maxwell', u'shadowpasswd': cpassword, u'aaa_access_unauth': [], 
@@ -65,15 +80,21 @@ if (args.add_user):
 		u'authkey': u'5b7d1ad4384332a0bd0030a4f82fc667989fe73d43b247a2f0dbce40', 
 		u'user': args.user_name, u'groups': [], u'crecord_name': args.user_name, 
 		u'aaa_owner': u'account.'+ args.user_name, u'aaa_access_other': [], u'_id': u'account.'+args.user_name}
-		collection.insert(post)
+		#collection.insert(post)
+		"""
+		post = caccount(user=args.user_name, group="capensis", mail=args.user_mail, )
+		my_record = caccount(post, storage=my_storage)
+		my_storage.put(my_record, namespace='object')
+		output = my_storage.find({'crecord_type':'account'},account=user_account)
+		print "len output = %s" % (len(output))
+
 	
 if (args.delete_user):
 	if not args.user_name:
 		print "args.user_name is None"
 		sys.exit(1)
 	else:
-		user_account = caccount(user="root", group="root")
-		my_storage = cstorage(user_account, namespace='object', logging_level=logging.DEBUG, mongo_host=args.server_link)
+
 		
 		my_record = my_storage.find({'user' : args.user_name})
 		my_storage.remove(my_record, account=user_account)
